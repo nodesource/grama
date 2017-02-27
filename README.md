ef3e560719a3f670b36082e8b80da5c29e3ffc4a
Thorsten Lorenz <thlorenz@gmx.de>
2017-02-27 13:30:31 -0600

    core: building ancestry

---
+++ README.md
@@ -0,0 +1,22 @@
+# grama [![build status](https://secure.travis-ci.org/nodesource/grama.png)](http://travis-ci.org/nodesource/grama)
+
+Pass grama an array of nodes with parent/child relationships and ask her questions about their ancestry.
+
+```js
+// TODO
+```
+
+## Status
+
+Nix, Nada, Nichevo, Nothing --> go away!
+
+## Installation
+
+    npm install grama
+
+## [API](https://nodesource.github.io/grama)
+
+
+## License
+
+MIT
---
+++ grama.js
@@ -0,0 +1,92 @@
+function lastMapEntry(map) {
+  let entry = null
+  const entries = map.entries()
+  while (true) {
+    const { done, value } = entries.next()
+    if (done) break
+    entry = value
+  }
+  return entry
+}
+
+class Grama {
+  constructor({ nodes, id, parentId }) {
+    this._nodes = this._mapOfNodes(nodes, id)
+    this._id = id
+    this._parentId = parentId
+    this._buildAncestry()
+  }
+
+  _mapOfNodes(nodes, id) {
+    function addToSet(acc, node) {
+      acc.set(node[id], node)
+      return acc
+    }
+    return nodes.reduce(addToSet, new Map())
+  }
+
+  _buildAncestry() {
+    this._ancestry = new Map()
+    // first determine direct parents
+    for (const [ id, val ] of this._nodes) {
+      const parent = val[this._parentId]
+      const info = { ancestors: new Map(), parent }
+      info.ancestors.set(parent, 1)
+      this._ancestry.set(id, info)
+    }
+
+    // Now iterate through the partially determined ancestry
+    // and determine more ancient ancestors.
+    // This algorithm is very simple as it processes one parent at a time
+    // and could be improved by consuming ancestors of the parent itself,
+    // but that'd be much more complex and error prone.
+    let keepGoing = true
+    while (keepGoing) {
+      keepGoing = false
+
+      for (const v of this._ancestry.values()) {
+        // find the most ancient ancestor we found so far and see if it itself has parent
+        const [ id, distance ] = lastMapEntry(v.ancestors)
+        // We may not know about that ancestor which means we are done with this child.
+        if (!this._ancestry.has(id)) continue
+
+        const grandParent = this._ancestry.get(id).parent
+        v.ancestors.set(grandParent, distance + 1)
+        keepGoing = true
+      }
+    }
+  }
+
+  get allNodes() {
+    return this._nodes
+  }
+
+  summary() {
+    return {
+        nodes: this.allNodes
+      , ancestry: this._ancestry
+    }
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true))
+}
+
+const grama = new Grama({
+    nodes: [
+        { type: 'FSREQWRAP', id: 10, tid: 3 }
+      , { type: 'FSREQWRAP', id: 11, tid: 3 }
+      , { type: 'TickObject', id: 12, tid: 3 }
+      , { type: 'FSREQWRAP', id: 13, tid: 11 }
+      , { type: 'FSREQWRAP', id: 14, tid: 13 }
+      , { type: 'FSREQWRAP', id: 15, tid: 13 }
+      , { type: 'TickObject', id: 16, tid: 13 }
+      , { type: 'FSREQWRAP', id: 18, tid: 15 }
+      , { type: 'FSREQWRAP', id: 19, tid: 15 } ]
+    , id: 'id'
+    , parentId: 'tid'
+})
+
+inspect(grama.summary())
